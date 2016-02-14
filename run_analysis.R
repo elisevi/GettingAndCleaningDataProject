a1be1202138096d83cb793836d3d25b8d85f9e74
elisevi <elise.vinhuba@gmail.com>
2016-02-14 22:44:41 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+
 # Load files
 dSTrain <- read.table("./UCI HAR Dataset/train/subject_train.txt");
 dXTrain <- read.table("./UCI HAR Dataset/train/X_train.txt");
@@ -16,19 +17,23 @@ data <- rbind(cbind(dSTrain,dYTrain,dXTrain),cbind(dSTest,dYTest,dXTest))
 names(data)<- c("Subject","Label",as.character(Feat$V2))
 
 # 2 Extracts only the measurements on the mean and standard deviation for each measurement. 
-extract_features <- grepl("mean|std", Feat$V2 )
-data_meanstd <- data[,c(1,1,extract_features)]
-names(data_meanstd)<- c("Subject","Label",as.character(Feat$V2[extract_features]))
+extract_features <- Feat[grep("mean\\(\\)|std\\(\\)", Feat$V2), ]
+data_meanstd <- data[,c(1,2,extract_features$V1+2)] 
+
 
 # 3 Uses descriptive activity names to name the activities in the data set
 data_meanstd$Label <- ActLab[data_meanstd$Label, 2]
 
 # 4 Appropriately labels the data set with descriptive variable names.
-names(data_meanstd) <- tolower(gsub("[^[:alpha:]]", "", names(data_meanstd)))
+# Already done in the last questions
 
 # 5 From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 agg_data <- aggregate(data_meanstd[, 3:ncol(data_meanstd)],
-                      by=list(subject = data_meanstd$subject, 
-                               label = data_meanstd$label), mean)
+                      by=list(Subject = data_meanstd$Subject, 
+                               Label = data_meanstd$Label), mean)
 
 write.table(format(agg_data, scientific=T), "tidy.txt", row.names=F, col.names=F, quote=2)
+
+
+
+
